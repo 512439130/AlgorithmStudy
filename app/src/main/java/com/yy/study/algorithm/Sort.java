90f9d35509ee7e3cc50d1b0106a42faab22e9c40
SoMustYY <13160677911@163.com>
2021-08-20 16:20:42 +0800

    update：1.排序稳定性整理。 2.希尔排序优化。

--- app/src/main/java/com/yy/study/algorithm/Sort.java
+++ app/src/main/java/com/yy/study/algorithm/Sort.java
@@ -5,9 +5,37 @@ import java.util.Random;
 
 public class Sort {
 
+    /**
+     * 交换数组元素顺序
+     * @param array  待排序数组
+     * @param indexA  位置A
+     * @param indexB  位置B
+     */
+    public static void swap(int[] array, int indexA, int indexB) {
+        int temp = array[indexA];
+        array[indexA] = array[indexB];
+        array[indexB] = temp;
+    }
+
+    public static boolean isSorted(int[] array) {
+        for (int i = 0; i < array.length; i++)
+            if (less(array[i], array[i - 1])) return false;
+        return true;
+    }
+
+    /**
+     * 是否排序
+     * @param v
+     * @param w
+     * @return
+     */
+    private static boolean less(int v, int w) {
+        return v < w;
+    }
+
     public static void main(String[] args) {
         int[] array = getRandomArrays(50000);
-//        System.out.println("待排序数组：" + Arrays.toString(array));
+        System.out.println("待排序数组：" + Arrays.toString(array));
         long startTime = System.currentTimeMillis();
           ///50000随机数
 //        popSort(array);    //8269ms
@@ -21,7 +49,7 @@ public class Sort {
 
         System.out.println("数组长度：" + array.length);
         System.out.println("排序总耗时：" + (endTime - startTime) + "ms");
-//        System.out.println("排序结果：" + Arrays.toString(array));
+        System.out.println("排序结果：" + Arrays.toString(array));
     }
 
     /**
@@ -40,6 +68,7 @@ public class Sort {
 
     /**
      * 冒泡排序（基础版）
+     * 稳定性：稳定
      * @param array 待排序数组
      */
     public static void popSort(int[] array) {
@@ -59,6 +88,7 @@ public class Sort {
 
     /**
      * 冒泡排序（优化版）
+     * 稳定性：稳定
      * 1.增加标志位，当内循环中无元素交换，则结束外层循环体
      * 2.记录内循环的元素交换，减少内层循环次数
      * @param array 待排序数组
@@ -95,6 +125,7 @@ public class Sort {
 
     /**
      * 选择排序
+     * 稳定性：非稳定
      * 每次找到数组中最小值的下标(从第1个下标开始，假设它是数组中最小的值，通过循环与全部元素比较，取最小值的下标)，将最小值下标与第1位下标替换，再寻找数组中剩下元素最小的值，再和数组第2位下标替换，一直执行，直到全部位置元素放置完成（从第1位一直排序到数组大小的位置为结束）
      * @param array 待排序数组
      */
@@ -114,39 +145,11 @@ public class Sort {
         }
     }
 
-    /**
-     * 交换数组元素顺序
-     * @param array  待排序数组
-     * @param indexA  位置A
-     * @param indexB  位置B
-     */
-    public static void swap(int[] array, int indexA, int indexB) {
-        int temp = array[indexA];
-        array[indexA] = array[indexB];
-        array[indexB] = temp;
-    }
-
-    public static boolean isSorted(int[] array) {
-        for (int i = 0; i < array.length; i++)
-            if (less(array[i], array[i - 1])) return false;
-        return true;
-    }
-
-    /**
-     * 是否排序
-     * @param v
-     * @param w
-     * @return
-     */
-    private static boolean less(int v, int w) {
-        return v < w;
-    }
 
     /**
      * 插入排序
-     * 升序排列
+     * 稳定性：稳定
      * 从数组左边开始，第一个位置有序，向右遍历，新元素和（左边）已有序位置数字挨个比较。如果当前比较者小于左边有序数组中的数，则向该位置插入，大于的元素统一向右移动，给新元素腾出空间
-     *
      * @param array 待排序数组
      */
     public static void insertSort(int[] array){
@@ -167,11 +170,12 @@ public class Sort {
 
     /**
      * 插入排序（优化版）
+     * 稳定性：稳定
      * 排序中不需数组交换元素，新元素和左边比较时，如果 array[j] < array[j-1]，则a[j] = array[j-1],更大的数向右覆盖，新元素等到最后合适的位置再放入
      * @param array 待排序数组
      */
     public static void insertSort2(int[] array){
-        if( array == null || array.length < 2) return;
+        if(array == null || array.length < 2) return;
         int len = array.length;
         int temp;
         for(int i = 1; i < len; i++){
@@ -195,40 +199,49 @@ public class Sort {
 
     /**
      * 高效排序
+     * 稳定性：非稳定
      * 希尔排序(基础：步长策略(折半)：n/2)
      * @param array 待排序数组
      */
     public static void shellSort(int[] array) {
+        if(array == null || array.length < 2) return;
         //步长
         int length = array.length;
         //缓存变量
         int temp;
         //求最大步长
         int step = length / 2;
-        while (step >= 1){
-//            System.out.println("step:" + step);
-            for (int i = step; i < length; i++) {
+        int i;
+        int j;
+        while (step > 0){
+            for (i = step; i < length; i++) {
                 temp = array[i];
-                int j = i - step;
-                while (j >= 0 && array[j] > temp) {
+                //1.根据步长计算左侧同组元素位置下标
+                j = i - step;
+                //2.同小组做直接插入排序（array[j + step] 和 array[j]）,比较满足条件后，调整2个元素顺序(1,2)
+                for (; (j >= 0 && array[j] > temp); j = j - step) {
+                    //1
                     array[j + step] = array[j];
-                    j = j - step;
                 }
+                //2
                 array[j + step] = temp;
-
             }
-            step /= 2;  //步长变化
+            //步长更新，逐步减少,最后step = 1 时 做最后的直接插入排序，保证排序的正确性
+            step /= 2;
         }
     }
 
     /**
      * 高效排序
+     * 稳定性：非稳定
      * 希尔排序(优化：步长策略：step = O(n^3/2) Knuth)
      * @param array 待排序数组
      */
     public static void shellSort2(int[] array) {
+        if(array == null || array.length < 2) return;
+        //顺序输出
         int length = array.length;
-//        int temp;
+        int temp;
         int step = 1;
         //求最大步长
         while (step < length / 3) {
@@ -237,28 +250,21 @@ public class Sort {
         int i;
         int j;
         while (step > 0) {
-//            System.out.println("step-update:" + step);
             for (i = step; i < length; i++) {
-                //根据步长计算左边同组元素位置下标
+                temp = array[i];
+                //1.根据步长计算左侧同组元素位置下标
                 j = i - step;
-                //比较同小组 1组n个元素，做同组的插入排序，每次比较满足条件后，调整2个元素顺序
-                while (j >= 0 && array[j] > array[i]) {
-                    //j:左边待比较元素
-                    //(j + step) 同组比较元素
-//                    System.out.println("update-j:"+ j);
-//                    System.out.println("update-step:"+ step);
-//                    System.out.println("update-array[j + step]:"+(j + step));
+                //2.同小组做直接插入排序（array[j + step] 和 array[j]）,比较满足条件后，调整2个元素顺序(1,2)
+                for (; j >= 0 && array[j] > temp; j = j - step) {
+                    //1
                     array[j + step] = array[j];
-
-                    //控制while循环跳出逻辑
-                    j = j - step;
                 }
-                //等价 array[0] = array[i];
-                array[j + step] = array[i];
-//                System.out.println("次排序后-array:"+Arrays.toString(array) + "  2个元素交换位置：array["+(j + step)+"]" + "==>>array["+(i)+"]");
+                //2
+                array[j + step] = temp;
+
             }
-            //步长更新
-            step = step / 3;
+            //步长更新，逐步减少,最后step = 1 时 做最后的直接插入排序，保证排序的正确性
+            step /= 3;
         }
 
     }

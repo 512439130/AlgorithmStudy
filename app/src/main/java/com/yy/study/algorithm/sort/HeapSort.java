1fe26dba4e76a50602c970f8d4cf6c6e6b806159
SoMustYY。 <13160677911@163.com>
2021-09-10 17:24:39 +0800

    update:堆排序编写。

---
+++ app/src/main/java/com/yy/study/algorithm/sort/HeapSort.java
@@ -0,0 +1,70 @@
+package com.yy.study.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * 堆排序
+ */
+public class HeapSort extends BaseSort{
+    /**
+     * 对树节点i堆化操作
+     * @param tree 堆的树形结构（数组）
+     * @param n 树的节点数：数组的总长度
+     * @param i 堆中树形结构的第i个节点
+     */
+    private void heapify(int[] tree,int n, int i){
+        if(i >= n){
+            return;
+        }
+        //求堆的最大值 parent（父节点）
+        int parent = i;
+        //当前i节点的左右节点
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if(left < n && tree[left] > tree[parent]){
+            parent = left;
+        }
+        if(right < n && tree[right] > tree[parent]){
+            parent = right;
+        }
+        if(parent != i){
+            //parent != i的时，（parent,left,right）中parent不是最大值，则parent 和 i的数组下标值进行交换 (parent保证是最大值)
+            swap(parent,i);
+            heapify(tree,n,parent);
+        }
+    }
+
+    /**
+     * 生成最大堆
+     * @param tree
+     * @param n
+     */
+    private void buildMaxHeap(int[] tree, int n){
+        //根据堆属性，通过最后一个叶子节点求最后一个叶子节点的父节点
+        //parent(i) = floor((i - 1) / 2)  floor 向下取证
+        int last = n - 1;
+        int parent = (last - 1) / 2;
+        for (int i = parent; i >= 0; i--) {
+            heapify(tree,n,i);
+        }
+    }
+
+    @Override
+    protected void sort() {
+        int length = array.length;
+        int treeParent = 0;
+        //生成最大堆，第0个数值为最大值
+        buildMaxHeap(array,length);
+        System.out.println("buildHeap:"+Arrays.toString(array));
+        for (int i = length - 1; i >= 0; i--) {
+            //数组的最大值array[0] 和数组最后位置交换
+            swap(i,treeParent);
+//          System.out.println("交换元素后：" + Arrays.toString(array) +" i:"+i + " treeParent:"+treeParent);
+            //对parent节点进行堆化
+            heapify(array,i,treeParent);
+//            System.out.println("堆化后：" + Arrays.toString(array));
+        }
+    }
+    //heapify i的含义：每次堆化后，最后一个节点（数组末尾）需要删减，所以数组长度跟随i变化
+    //heapify treeParent的含义：数组交换后，此时array[0]是替换后的原本数组末尾的数值，进行下一次堆化
+}
